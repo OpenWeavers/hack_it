69755800b82b53642f0a8591cda9a133171634e9
Sourabh Daroji <darojisourabh@ymail.com>
2018-01-20 23:30:52 +0530

    Commented out materializecss.
    Since Main page will not use it.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-amber.min.css" />
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-        <link rel="stylesheet" href="css/materialize.css" >
+        <!--<link rel="stylesheet" href="css/materialize.css" >-->
         <link rel="stylesheet" href="css/main.css">
         <style>
             .demo-card-wide.mdl-card {
