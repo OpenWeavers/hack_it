c40623b30a2a48a66180dc7606ec9fd5ae6dc862
GiZmoFalcon <darojisourabh@ymail.com>
2018-01-20 23:10:00 +0530

    Added UI to Landing Page

--- index.php
+++ index.php
@@ -9,9 +9,24 @@
 
         <link rel="manifest" href="site.webmanifest">
         <!-- Place favicon.ico in the root directory -->
-
-        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-amber.min.css" />
+        <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+        <link rel="stylesheet" href="css/materialize.css" >
         <link rel="stylesheet" href="css/main.css">
+        <style>
+            .demo-card-wide.mdl-card {
+                width: 512px;
+            }
+            .demo-card-wide > .mdl-card__title {
+                color: #fff;
+                height: 176px;
+                background: url('./img/hacking-icon-clipart-png-21.png') center / cover;
+            }
+            .demo-card-wide > .mdl-card__menu {
+                color: #fff;
+            }
+        </style>
     </head>
     <body>
         <!--[if lte IE 9]>
@@ -19,9 +34,63 @@
         <![endif]-->
 
         <!-- Add your site or application content here -->
-        <p>Hello world!</p>
+        <!-- Always shows a header, even in smaller screens. -->
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+            <header class="mdl-layout__header">
+                <div class="mdl-layout__header-row">
+                    <!-- Title -->
+                    <span class="mdl-layout-title">hack_it</span>
+                    <!-- Add spacer, to align navigation to the right -->
+                    <div class="mdl-layout-spacer"></div>
+                    <!-- Navigation. We hide it in small screens. -->
+                    <nav class="mdl-navigation mdl-layout--large-screen-only">
+                        <a class="mdl-navigation__link" href="">Leader Board</a>
+                        <a class="mdl-navigation__link" href="">r/hack_it</a>
+                        <a class="mdl-navigation__link" href="">About</a>
+                        <a class="mdl-navigation__link" href="">Sign In</a>
+                    </nav>
+                </div>
+            </header>
+            <div class="mdl-layout__drawer">
+                <span class="mdl-layout-title">Username here</span>
+                <nav class="mdl-navigation">
+                    <a class="mdl-navigation__link" href="">Leader Board</a>
+                    <a class="mdl-navigation__link" href="">r/hack_it</a>
+                    <a class="mdl-navigation__link" href="">About</a>
+                    <a class="mdl-navigation__link" href="">Sign In</a>
+                </nav>
+            </div>
+            <main class="mdl-layout__content">
+                <div class="page-content">
 
 
+                        <div class="row">
+                            <div class=" col s6 m6" style="position: relative;">
+                                <object width="800" height="800" data="./404.html" style="margin: 0 auto; position: absolute; top: 50%; transform: translateY(+5%);" class="responsive"></object>
+                            </div>
+                            <div class="row">
+                                <!-- Wide card with share menu button -->
+
+
+
+                                <div class="demo-card-wide mdl-card mdl-shadow--2dp col s6 m6" style="position: relative;">
+                                    <div class="mdl-card__title">
+                                        <h2 class="mdl-card__title-text" style="color: black">Welcome</h2>
+                                    </div>
+                                    <div class="mdl-card__supporting-text">
+                                        An Online Event, that is as simple as just filling text in an Input Field, but involves a lot of tweaks and hacks of varying difficulty to get to the answer. The questions will be framed such that, the participants will gain good amount of basic hacking skills and also an exposure to places to get started with such stuff and also some cryptic puzzles.
+                                    </div>
+                                    <div class="mdl-card__actions mdl-card--border">
+                                        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                                            Sign Up
+                                        </a>
+                                    </div>
+                                </div></div>
+
+                        </div>
+                    </div>
+            </main>
+        </div>
 
     </body>
 </html>
