ffb8ccbc0a09a1a91d6ec880286179141968a49c
Vinyas N S <vinyas.n.s@gmail.com>
2018-01-23 00:51:44 +0530

    handle block status

--- levels/1.php
+++ levels/1.php
@@ -1,13 +1,23 @@
 <?php
 require '../com/config/DBHelper.php';
 session_start();
+$level = basename($_SERVER['SCRIPT_FILENAME'], ".php");
 if (!isset($_SESSION['username']))  {
     header("location:../index.php");
 }
-if (isset($_SESSION['username']) && $_SESSION['current_level'] != 1) {
+if (isset($_SESSION['username']) && $_SESSION['current_level'] != $level) {
     header("location:" . $_SESSION['current_level'] . ".php");
 }
-
+$now = date('Y-m-d H:i:s');
+$time_to_unblock = date($_SESSION['when_to_unblock']);
+if ($_SESSION['on_block'] == $level && $time_to_unblock < $now) {
+    $db = new DBHelper();
+    $con = $db->getConnection();
+    $username = $_SESSION['username'];
+    $query = "UPDATE track_records SET on_block=0 WHERE username='$username'";
+    $con->query($query);
+    $_SESSION['on_block'] = 0;
+}
 ?>
 <!doctype html>
 <html lang="">
