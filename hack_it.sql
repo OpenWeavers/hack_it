56a95ea2fdd06995aaffe1bc21286ad588322ed4
Vinyas N S <vinyas.n.s@gmail.com>
2018-01-25 22:51:49 +0530

    update Database design

--- hack_it.sql
+++ hack_it.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.2
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 21, 2018 at 06:33 AM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.8
+-- Generation Time: Jan 25, 2018 at 05:19 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -35,6 +35,14 @@ CREATE TABLE `questions` (
   `points` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`question_no`, `hint`, `answer`, `points`) VALUES
+(1, 'hint', '1', 10),
+(2, 'no hint', 'hello world', 20);
+
 -- --------------------------------------------------------
 
 --
@@ -47,8 +55,17 @@ CREATE TABLE `track_records` (
   `total_score` int(11) NOT NULL DEFAULT '0',
   `current_hint_took` int(11) NOT NULL DEFAULT '0',
   `on_block` int(11) NOT NULL DEFAULT '0',
-  `when_to_unblock` datetime NOT NULL
+  `when_to_unblock` datetime NOT NULL,
+  `last_success` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `track_records`
+--
+
+INSERT INTO `track_records` (`username`, `current_level`, `total_score`, `current_hint_took`, `on_block`, `when_to_unblock`, `last_success`) VALUES
+('sas', 1, 10, 0, 0, '2018-01-25 06:41:54', '2018-01-25 06:44:29'),
+('sourabh', 2, 20, 0, 0, '2018-01-25 06:41:14', '2018-01-25 17:07:41');
 
 -- --------------------------------------------------------
 
@@ -72,8 +89,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `email`, `password`, `phone`, `college`, `activated`, `temp_pwd`, `hash`) VALUES
-('2', 'cesufufo@20boxme.org', 'Tgbyhnujm', 1231231231, '6', 1, 'Tgbyhnujm', '8402833795a64232448a6b'),
-('Wart', 'temproary3@gmail.com', 'qwe', 234, 'QAZ', 0, 'qwe', NULL);
+('sas', 'sas@sas.com', '8ad17dadafdf341124084e302023fc75bc5cf7b265b7ff84e383a5da182aa7f48d70e9e70b96b0045c10c911dadbcff9817ab7b1760c5366e6df3f33af5fc51c', 1234567890, 'sjce', 1, NULL, '19305891765a697c2e3ff68'),
+('sourabh', 'ss@ss.com', '8ad17dadafdf341124084e302023fc75bc5cf7b265b7ff84e383a5da182aa7f48d70e9e70b96b0045c10c911dadbcff9817ab7b1760c5366e6df3f33af5fc51c', 1234567890, 'sjce', 1, NULL, '17588021755a697c076cd86');
 
 --
 -- Indexes for dumped tables
