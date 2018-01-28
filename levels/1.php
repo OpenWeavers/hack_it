6fcc5afdd1e6ff41c8c62b39927c848472177027
sytays <syedtysh@gmail.com>
2018-01-28 16:45:57 +0530

    Added hint button and reformatted code

--- levels/1.php
+++ levels/1.php
@@ -8,10 +8,10 @@ if (!isset($_SESSION['username'])) {
 if (isset($_SESSION['username']) && $_SESSION['current_level'] != $level) {
     header("location:" . $_SESSION['current_level'] . ".php");
 }
-$now = date('Y-m-d H:i:s');
-$time_to_unblock = date($_SESSION['when_to_unblock']);
 if ($_SESSION['on_block'] == $level) {
-    if ($time_to_unblock < $now) {
+    $now = date('Y-m-d H:i:s');
+    $time_to_unblock = date($_SESSION['when_to_unblock']);
+    if ($time_to_unblock <= $now) {
         $db = new DBHelper();
         $con = $db->getConnection();
         $username = $_SESSION['username'];
@@ -25,6 +25,7 @@ if ($_SESSION['on_block'] == $level) {
 ?>
 <!doctype html>
 <html lang="">
+<!-- password: lccfossbytes2018 -->
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -33,23 +34,51 @@ if ($_SESSION['on_block'] == $level) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="manifest" href="../site.webmanifest">
-    <!-- Place favicon.ico in the root directory -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-amber.min.css"/>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <!--<link rel="stylesheet" href="../css/materialize.css" >-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="../css/materialize.min.css" media="screen,projection"/>
     <link rel="stylesheet" href="../css/main.css">
+    <style>
+        .toast {
+            width: 50%;
+            border-radius: 0;
+        }
+
+        #toast-container {
+            min-width: 100%;
+            bottom: 0%;
+            top: 70%;
+            right: 0%;
+            left: 25%;
+        }
+    </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="../js/materialize.min.js"></script>
     <script>
         $(document).ready(function () {
             $(".button-collapse").sideNav();
+
+            $("#hntbtn").click(function (e) {
+                e.preventDefault();
+                $.ajax({
+                    type: "POST",
+                    url: "get_hint.php",
+                    data: {},
+                    success: function (result) {
+                        var $toastContent = '<span style="word-wrap: break-word">' + result + '</span><button class="btn-flat toast-action" onclick="dismissToast()">Dismiss</button>';
+                        Materialize.toast($toastContent, 100000);
+                    },
+                    error: function (result) {
+                    }
+                });
+            });
         });
 
+        function dismissToast() {
+            Materialize.Toast.removeAll();
+        }
     </script>
-
 </head>
 <body>
 <!--[if lte IE 9]>
@@ -57,8 +86,6 @@ if ($_SESSION['on_block'] == $level) {
     your browser</a> to improve your experience and security.</p>
 <![endif]-->
 
-<!-- Add your site or application content here -->
-<!-- Always shows a header, even in smaller screens. -->
 <nav>
     <div class="nav-wrapper">
         <a href="#!" class="brand-logo">Hack_It</a>
@@ -81,13 +108,13 @@ if ($_SESSION['on_block'] == $level) {
     <form class="col s6" action="answer_verification.php" method="post">
 
         <div class="row">
-            <!--Put Question Content Here!-->
             <div class="input-field col s12">
                 <input name="answer" id="input1" class="input-field inline" type="text">
-                <br />
+                <br/>
                 <label for="input1">Answer</label>
             </div>
         </div>
+        <button class="btn waves-effect waves-light" id="hntbtn">Hint ?</button>
         <button class="btn waves-effect waves-light" type="submit" name="action">Submit
             <i class="material-icons right">send</i>
         </button>
